f6dd4bf597befb6459d4dfc12b795843f8ad406f
Craig Blanchette <craig.blanchette@gmail.com>
2013-03-26 22:10:46 -0400

    Formatting

--- jquery.ajaxsubmit.js
+++ jquery.ajaxsubmit.js
@@ -1,52 +1,50 @@
-(function($){
+(function($) {
 
-	/**
-	*  jQuery ajaxSubmit
-	*  Craig Blanchette (isometriks.com)
-	*  craig.blanchette@gmail.com
-	*
-	*  $('form').ajaxSubmit(function(Object data, String textStatus, jqXHR jqXHR){ ... });
-	*/
-	$.fn.ajaxSubmit = function(success, options){
+    /**
+     *  jQuery ajaxSubmit
+     *  Craig Blanchette (isometriks.com)
+     *  craig.blanchette@gmail.com
+     *
+     *  $('form').ajaxSubmit(function(Object data, String textStatus, jqXHR jqXHR){ ... });
+     */
+    $.fn.ajaxSubmit = function(success, options) {
 
-		var default_options = {
-			url: this.attr('action') || location.href, 
-			success: success, 
-			type: this.attr('method') || 'POST'
-		};
+        var default_options = {
+            url: this.attr('action') || location.href,
+            success: success,
+            type: this.attr('method') || 'POST'
+        };
 
-		var options = $.extend(default_options, options || {}); 
+        var options = $.extend(default_options, options || {});
 
-		if(!!this.attr('enctype') && this.attr('enctype').toLowerCase() === 'multipart/form-data'){
+        if (!!this.attr('enctype') && this.attr('enctype').toLowerCase() === 'multipart/form-data') {
 
-			var formData = new FormData(); 
+            var formData = new FormData();
 
-			var $files = this.find('input[type="file"][name]'); 
-			$files.each(function(){
-				if('files' in this && this.files.length > 0){
-					// ToDo: Support Multiple on any input? 
-					// Just need a loop here..
-					formData.append(this.name, this.files[0]);
-			    }
-			});
+            var $files = this.find('input[type="file"][name]');
+            $files.each(function() {
+                if ('files' in this && this.files.length > 0) {
+                    // ToDo: Support Multiple on any input? 
+                    // Just need a loop here..
+                    formData.append(this.name, this.files[0]);
+                }
+            });
 
-			var $noFiles = this.find(':not(input[type="file"])'); 
-			$.each($noFiles.serializeArray(), function(i, pair){
-				formData.append(pair.name, pair.value); 
-			}); 
+            var $noFiles = this.find(':not(input[type="file"])');
+            $.each($noFiles.serializeArray(), function(i, pair) {
+                formData.append(pair.name, pair.value);
+            });
 
-			options.data = formData; 
-			options.method = 'POST'; 
-			options.contentType = false; 
-			options.processData = false; 
-			
-		} else {
+            options.data = formData;
+            options.method = 'POST';
+            options.contentType = false;
+            options.processData = false;
 
-			options.data = this.serializeArray(); 
+        } else {
+            options.data = this.serializeArray();
+        }
 
-		}
-
-		$.ajax(options);
-	}
+        $.ajax(options);
+    }
 
 })(jQuery); 
